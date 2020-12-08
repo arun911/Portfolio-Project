1d5e54c06708f4dfb8d12ba570a8d6b02cc5b26e
arun911 <arun1935raj911@gmail.com>
2020-12-08 16:19:27 +0530

    Add all my files

--- Jobs/models.py
+++ Jobs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Job(models.Model):
     Image =models.ImageField(upload_to="images/")
+
     Title=models.CharField(max_length=200)
 
     Summary=models.CharField(max_length=200)
